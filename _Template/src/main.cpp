5993b6996362e0e7c266ba984b9ad5d8b85fc255
Hiram Montejano Gómez <hiram.montejano@gmail.com>
2022-10-25 11:56:13 +0200

    Updated C Template. Added a C++ Template

---
+++ _Template/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * @FILENAME:           main.cpp
+ *
+ * @DESCRIPTION:        ..
+ *
+ * @HARDWARE:           ..
+ *
+ * @AUTHOR:             Hiram Montejano Gómez
+ * 
+ * @STARTING DATE:      ..
+ *
+ * @ENDING DATE:        ..
+ */
+
+#include <TM4C123GH6PM.h>
+
+int main(void)
+{
+
+
+  return 0;
+}
