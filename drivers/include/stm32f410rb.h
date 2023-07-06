a391696c055b6a76c113bdc464f35621d54a7a53
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-07-06 08:05:36 +0200

    added comments

--- drivers/include/stm32f410rb.h
+++ drivers/include/stm32f410rb.h
@@ -38,33 +38,31 @@
 #define     __IO    volatile             // Defines 'read / write' permissions
 
 
-// TODO missing comments
 /****************************** SYSCFG ******************************/
 /**
  * @brief       SYSCFG register structure
  */
 typedef struct {
-  __IO uint32_t MEMRMP;
-  __IO uint32_t PMC;
-  __IO uint32_t EXTICR[4];
-  __IO uint32_t CFGR2;
-  __IO uint32_t CMPCR;
-  __IO uint32_t CFGR;
+  __IO uint32_t MEMRMP;         // Memory remap
+  __IO uint32_t PMC;            // Peripheral mode configuration
+  __IO uint32_t EXTICR[4];      // External interrupt configuration [1..4]
+  __IO uint32_t CFGR2;          // Configuration register 2
+  __IO uint32_t CMPCR;          // Compensation cell control
+  __IO uint32_t CFGR;           // Configuration register
 } SYSCFG_Type;
 
 
-// TODO missing comments
 /****************************** EXTI ******************************/
 /**
  * @brief       EXTI register structure
  */
 typedef struct {
-  __IO uint32_t IMR;
-  __IO uint32_t EMR;
-  __IO uint32_t RTSR;
-  __IO uint32_t FTSR;
-  __IO uint32_t SWIER;
-  __IO uint32_t PR;
+  __IO uint32_t IMR;            // Interrupt mask
+  __IO uint32_t EMR;            // Event mask
+  __IO uint32_t RTSR;           // Rising trigger selection
+  __IO uint32_t FTSR;           // Falling trigger selection
+  __IO uint32_t SWIER;          // Software interrupt event
+  __IO uint32_t PR;             // Pending
 } EXTI_Type;
 
 
@@ -73,16 +71,16 @@ typedef struct {
  * @brief       GPIO register structure
  */
 typedef struct {
-    __IO    uint32_t MODER;     // Mode
-    __IO    uint32_t OTYPER;    // Output type
-    __IO    uint32_t OSPEEDR;   // Output speed
-    __IO    uint32_t PUPDR;     // Pull-up/pull-down
-    __I     uint32_t IDR;       // Input data
-    __O     uint32_t ODR;       // Output data
-    __IO    uint32_t BSRR;      // Bit set/reset
-    __IO    uint32_t LCKR;      // Configuration lock
-    __IO    uint32_t AFRL;      // Alternate function low
-    __IO    uint32_t AFRH;      // Alternate function high
+  __IO    uint32_t MODER;       // Mode
+  __IO    uint32_t OTYPER;      // Output type
+  __IO    uint32_t OSPEEDR;     // Output speed
+  __IO    uint32_t PUPDR;       // Pull-up/pull-down
+  __I     uint32_t IDR;         // Input data
+  __O     uint32_t ODR;         // Output data
+  __IO    uint32_t BSRR;        // Bit set/reset
+  __IO    uint32_t LCKR;        // Configuration lock
+  __IO    uint32_t AFRL;        // Alternate function low
+  __IO    uint32_t AFRH;        // Alternate function high
 } GPIO_Type;
 
 
@@ -91,54 +89,49 @@ typedef struct {
  * @brief       RCC register structure
  */
 typedef struct {
-    __IO uint32_t CR;           // Clock control
-    __IO uint32_t PLLCFGR;      // PLL configuration
-    __IO uint32_t CFGR;         // Clock configuration
-    __IO uint32_t CIR;          // Clock interrupt
-    __IO uint32_t AHB1RSTR;     // AHB1 peripheral reset
-    __I  uint32_t RESERVED1[3];
-    __IO uint32_t APB1RSTR;     // APB1 peripheral reset
-    __IO uint32_t APB2RSTR;     // APB2 peripheral reset
-    __I  uint32_t RESERVED2[2];
-    __IO uint32_t AHB1ENR;      // AHB1 peripheral clock enable
-    __I  uint32_t RESERVED3[3];
-    __IO uint32_t APB1ENR;      // APB1 peripheral clock enable
-    __IO uint32_t APB2ENR;      // APB2 peripheral clock enable
-    __I  uint32_t RESERVED4[2];
-    __IO uint32_t AHB1LPENR;    // AHB1 peripheral clock enable in low power mode
-    __I  uint32_t RESERVED5[3];
-    __IO uint32_t APB1LPENR;    // APB1 peripheral clock enable in low power mode
-    __IO uint32_t APB2LPENR;    // APB2 peripheral clock enable in low power mode
-    __I  uint32_t RESERVED6[2];
-    __IO uint32_t BDCR;         // Backup domain control
-    __IO uint32_t CSR;          // Clock control & status
-    __I  uint32_t RESERVED7[2];
-    __IO uint32_t SSCGR;        // Spread spectrum clock generation
-    __I  uint32_t RESERVED8[2];
-    __IO uint32_t DCKCFGR;      // Dedicated Clocks Configuration
-    __I  uint32_t RESERVED9;
-    __IO uint32_t DCKCFGR2;     // Dedicated Clocks Configuration 2
+  __IO uint32_t CR;             // Clock control
+  __IO uint32_t PLLCFGR;        // PLL configuration
+  __IO uint32_t CFGR;           // Clock configuration
+  __IO uint32_t CIR;            // Clock interrupt
+  __IO uint32_t AHB1RSTR;       // AHB1 peripheral reset
+  __I  uint32_t RESERVED1[3];
+  __IO uint32_t APB1RSTR;       // APB1 peripheral reset
+  __IO uint32_t APB2RSTR;       // APB2 peripheral reset
+  __I  uint32_t RESERVED2[2];
+  __IO uint32_t AHB1ENR;        // AHB1 peripheral clock enable
+  __I  uint32_t RESERVED3[3];
+  __IO uint32_t APB1ENR;        // APB1 peripheral clock enable
+  __IO uint32_t APB2ENR;        // APB2 peripheral clock enable
+  __I  uint32_t RESERVED4[2];
+  __IO uint32_t AHB1LPENR;      // AHB1 peripheral clock enable in low power mode
+  __I  uint32_t RESERVED5[3];
+  __IO uint32_t APB1LPENR;      // APB1 peripheral clock enable in low power mode
+  __IO uint32_t APB2LPENR;      // APB2 peripheral clock enable in low power mode
+  __I  uint32_t RESERVED6[2];
+  __IO uint32_t BDCR;           // Backup domain control
+  __IO uint32_t CSR;            // Clock control & status
+  __I  uint32_t RESERVED7[2];
+  __IO uint32_t SSCGR;          // Spread spectrum clock generation
+  __I  uint32_t RESERVED8[2];
+  __IO uint32_t DCKCFGR;        // Dedicated Clocks Configuration
+  __I  uint32_t RESERVED9;
+  __IO uint32_t DCKCFGR2;       // Dedicated Clocks Configuration 2
 } RCC_Type;
 
 
 /****************************** NVIC ******************************/
 /**
  * @brief       NVIC register structure
+ * 
+ * @note        STIR register is located in a separate block at 0xE000EF00
  */
 typedef struct {
-  __IO uint32_t ISER[3];        // Interrupt set-enable
-  __I  uint32_t RESERVED1[5];
-  __IO uint32_t ICER[3];        // Interrupt clear-enable
-  __I  uint32_t RESERVED2[5];
-  __IO uint32_t ISPR[3];        // Interrupt set-pending
-  __I  uint32_t RESERVED3[5];
-  __IO uint32_t ICPR[3];        // Interrupt clear-pending
-  __I  uint32_t RESERVED4[5];
-  __IO uint32_t IABR[3];        // Interrupt active bit
-  __I  uint32_t RESERVED5[5];
+  __IO uint32_t ISER[8];        // Interrupt set-enable
+  __IO uint32_t ICER[8];        // Interrupt clear-enable
+  __IO uint32_t ISPR[8];        // Interrupt set-pending
+  __IO uint32_t ICPR[8];        // Interrupt clear-pending
+  __IO uint32_t IABR[8];        // Interrupt active bit
   __IO uint32_t IPR[60];        // Interrupt priority 
-  __I  uint32_t RESERVED6[5];
-  __O  uint32_t STIR;           // Software trigger interrupt
 } NVIC_Type;
 
 
@@ -157,12 +150,12 @@ typedef struct {
 /********************* Peripheral Declaration ********************/
 
 #define SYSCFG              ((SYSCFG_Type*) SYSCFG_BASE_ADDR)
-#define EXTI                ((EXTI_Type*) EXTI_BASE_ADDR)
-#define GPIOA               ((GPIO_Type*) GPIOA_BASE_ADDR)
-#define GPIOB               ((GPIO_Type*) GPIOB_BASE_ADDR)
-#define GPIOC               ((GPIO_Type*) GPIOC_BASE_ADDR)
-#define GPIOH               ((GPIO_Type*) GPIOH_BASE_ADDR)
-#define RCC                 ((RCC_Type*) RCC_BASE_ADDR)
-#define NVIC                ((NVIC_Type*) NVIC_BASE_ADDR)
+#define EXTI                ((EXTI_Type*)   EXTI_BASE_ADDR)
+#define GPIOA               ((GPIO_Type*)   GPIOA_BASE_ADDR)
+#define GPIOB               ((GPIO_Type*)   GPIOB_BASE_ADDR)
+#define GPIOC               ((GPIO_Type*)   GPIOC_BASE_ADDR)
+#define GPIOH               ((GPIO_Type*)   GPIOH_BASE_ADDR)
+#define RCC                 ((RCC_Type*)    RCC_BASE_ADDR)
+#define NVIC                ((NVIC_Type*)   NVIC_BASE_ADDR)
 
 #endif
