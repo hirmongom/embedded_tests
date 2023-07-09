5862655fb07147103ebfb29cb949cceb48b38f06
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-07-09 08:07:46 +0200

    separated interrupts NMI, hardfault, memmanage, busfault and usagefault from overridable isrs

--- startup/startup.c
+++ startup/startup.c
@@ -43,11 +43,11 @@
 void Default_ISR(void);
 void Reset_ISR(void);
 
-void NMI_ISR(void)                      OVERRIDABLE_ISR;
-void HardFault_ISR(void)                OVERRIDABLE_ISR;
-void MemManage_ISR(void)                OVERRIDABLE_ISR;
-void BusFault_ISR(void)                 OVERRIDABLE_ISR;
-void UsageFault_ISR(void)               OVERRIDABLE_ISR;
+void NMI_ISR(void);
+void HardFault_ISR(void);
+void MemManage_ISR(void);
+void BusFault_ISR(void);
+void UsageFault_ISR(void);
 void SVCall_ISR(void)                   OVERRIDABLE_ISR;
 void DebugMonitor_ISR(void)             OVERRIDABLE_ISR;
 void PendSV_ISR(void)                   OVERRIDABLE_ISR;
@@ -126,121 +126,121 @@ void EXTI23_LPTIM1_ISR(void)            OVERRIDABLE_ISR;
  * @see RM0401 Reference Manual, Page 198 for more information on the NVIC and the vector table.
  */
 uint32_t NVIC[] __attribute__((section(".isr_vector"))) = {
-    STACK_START,
-    (uint32_t)&Reset_ISR,
-    (uint32_t)&NMI_ISR,
-    (uint32_t)&HardFault_ISR,
-    (uint32_t)&MemManage_ISR,
-    (uint32_t)&BusFault_ISR,
-    (uint32_t)&UsageFault_ISR,
-    0,
-    0,
-    0,
-    0,
-    (uint32_t)&SVCall_ISR,
-    (uint32_t)&DebugMonitor_ISR,
-    0,
-    (uint32_t)&PendSV_ISR,
-    (uint32_t)&Systick_ISR,
-    (uint32_t)&WWDG_ISR,
-    (uint32_t)&PVD_ISR,
-    (uint32_t)&EXTI21_TAMP_STAMP_ISR,
-    (uint32_t)&EXTI22_RTC_WKUP_ISR,
-    (uint32_t)&FLASH_ISR,
-    (uint32_t)&RCC_ISR,
-    (uint32_t)&EXTI0_ISR,
-    (uint32_t)&EXTI1_ISR,
-    (uint32_t)&EXTI2_ISR,
-    (uint32_t)&EXTI3_ISR,
-    (uint32_t)&EXTI4_ISR,
-    (uint32_t)&DMA1_Stream0_ISR,
-    (uint32_t)&DMA1_Stream1_ISR,
-    (uint32_t)&DMA1_Stream2_ISR,
-    (uint32_t)&DMA1_Stream3_ISR,
-    (uint32_t)&DMA1_Stream4_ISR,
-    (uint32_t)&DMA1_Stream5_ISR,
-    (uint32_t)&DMA1_Stream6_ISR,
-    (uint32_t)&ADC_ISR,
-    0,
-    0,
-    0,
-    0,
-    (uint32_t)&EXTI9_5_ISR,
-    (uint32_t)&TIM1_BRK_TIM9_ISR,
-    (uint32_t)&TIM1_UP_ISR,
-    (uint32_t)&TIM1_TRG_COM_TIM11_ISR,
-    (uint32_t)&TIM1_CC_ISR,
-    0,
-    0,
-    0,
-    (uint32_t)&I2C1_EV_ISR,
-    (uint32_t)&I2C1_ER_ISR,
-    (uint32_t)&I2C2_EV_ISR,
-    (uint32_t)&I2C2_ER_ISR,
-    (uint32_t)&SPI1_ISR,
-    (uint32_t)&SPI2_ISR,
-    (uint32_t)&USART1_ISR,
-    (uint32_t)&USART2_ISR,
-    0,
-    (uint32_t)&EXTI15_10_ISR,
-    (uint32_t)&EXTI17_RTC_Alarm_ISR,
-    0,
-    0,
-    0,
-    0,
-    0,
-    (uint32_t)&DMA1_Stream7_ISR,
-    0,
-    0,
-    (uint32_t)&TIM5_ISR,
-    0,
-    0,
-    0,
-    (uint32_t)&TIM6_DAC_ISR,
-    0,
-    (uint32_t)&DMA2_Stream0_ISR,
-    (uint32_t)&DMA2_Stream1_ISR,
-    (uint32_t)&DMA2_Stream2_ISR,
-    (uint32_t)&DMA2_Stream3_ISR,
-    (uint32_t)&DMA2_Stream4_ISR,
-    0,
-    (uint32_t)&EXTI19_ISR,
-    0,
-    0,
-    0,
-    0,
-    0,
-    (uint32_t)&DMA2_Stream5_ISR,
-    (uint32_t)&DMA2_Stream6_ISR,
-    (uint32_t)&DMA2_Stream7_ISR,
-    (uint32_t)&USART6_ISR,
-    0,
-    0,
-    0,
-    0,
-    (uint32_t)&EXTI20_ISR,
-    0,
-    0,
-    0,
-    (uint32_t)&RNG_ISR,
-    (uint32_t)&FPU_ISR,
-    0,
-    0,
-    0,
-    (uint32_t)&SPI5_ISR,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    (uint32_t)&I2C4_EV_ISR,
-    (uint32_t)&I2C4_ER_ISR,
-    (uint32_t)&EXTI23_LPTIM1_ISR
+  STACK_START,
+  (uint32_t)&Reset_ISR,
+  (uint32_t)&NMI_ISR,
+  (uint32_t)&HardFault_ISR,
+  (uint32_t)&MemManage_ISR,
+  (uint32_t)&BusFault_ISR,
+  (uint32_t)&UsageFault_ISR,
+  0,
+  0,
+  0,
+  0,
+  (uint32_t)&SVCall_ISR,
+  (uint32_t)&DebugMonitor_ISR,
+  0,
+  (uint32_t)&PendSV_ISR,
+  (uint32_t)&Systick_ISR,
+  (uint32_t)&WWDG_ISR,
+  (uint32_t)&PVD_ISR,
+  (uint32_t)&EXTI21_TAMP_STAMP_ISR,
+  (uint32_t)&EXTI22_RTC_WKUP_ISR,
+  (uint32_t)&FLASH_ISR,
+  (uint32_t)&RCC_ISR,
+  (uint32_t)&EXTI0_ISR,
+  (uint32_t)&EXTI1_ISR,
+  (uint32_t)&EXTI2_ISR,
+  (uint32_t)&EXTI3_ISR,
+  (uint32_t)&EXTI4_ISR,
+  (uint32_t)&DMA1_Stream0_ISR,
+  (uint32_t)&DMA1_Stream1_ISR,
+  (uint32_t)&DMA1_Stream2_ISR,
+  (uint32_t)&DMA1_Stream3_ISR,
+  (uint32_t)&DMA1_Stream4_ISR,
+  (uint32_t)&DMA1_Stream5_ISR,
+  (uint32_t)&DMA1_Stream6_ISR,
+  (uint32_t)&ADC_ISR,
+  0,
+  0,
+  0,
+  0,
+  (uint32_t)&EXTI9_5_ISR,
+  (uint32_t)&TIM1_BRK_TIM9_ISR,
+  (uint32_t)&TIM1_UP_ISR,
+  (uint32_t)&TIM1_TRG_COM_TIM11_ISR,
+  (uint32_t)&TIM1_CC_ISR,
+  0,
+  0,
+  0,
+  (uint32_t)&I2C1_EV_ISR,
+  (uint32_t)&I2C1_ER_ISR,
+  (uint32_t)&I2C2_EV_ISR,
+  (uint32_t)&I2C2_ER_ISR,
+  (uint32_t)&SPI1_ISR,
+  (uint32_t)&SPI2_ISR,
+  (uint32_t)&USART1_ISR,
+  (uint32_t)&USART2_ISR,
+  0,
+  (uint32_t)&EXTI15_10_ISR,
+  (uint32_t)&EXTI17_RTC_Alarm_ISR,
+  0,
+  0,
+  0,
+  0,
+  0,
+  (uint32_t)&DMA1_Stream7_ISR,
+  0,
+  0,
+  (uint32_t)&TIM5_ISR,
+  0,
+  0,
+  0,
+  (uint32_t)&TIM6_DAC_ISR,
+  0,
+  (uint32_t)&DMA2_Stream0_ISR,
+  (uint32_t)&DMA2_Stream1_ISR,
+  (uint32_t)&DMA2_Stream2_ISR,
+  (uint32_t)&DMA2_Stream3_ISR,
+  (uint32_t)&DMA2_Stream4_ISR,
+  0,
+  (uint32_t)&EXTI19_ISR,
+  0,
+  0,
+  0,
+  0,
+  0,
+  (uint32_t)&DMA2_Stream5_ISR,
+  (uint32_t)&DMA2_Stream6_ISR,
+  (uint32_t)&DMA2_Stream7_ISR,
+  (uint32_t)&USART6_ISR,
+  0,
+  0,
+  0,
+  0,
+  (uint32_t)&EXTI20_ISR,
+  0,
+  0,
+  0,
+  (uint32_t)&RNG_ISR,
+  (uint32_t)&FPU_ISR,
+  0,
+  0,
+  0,
+  (uint32_t)&SPI5_ISR,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  (uint32_t)&I2C4_EV_ISR,
+  (uint32_t)&I2C4_ER_ISR,
+  (uint32_t)&EXTI23_LPTIM1_ISR
 };
 
 extern uint32_t _etext;
@@ -267,54 +267,74 @@ int main(void);
  * by setting it to zero, and then calling the user-defined main function.
  */
 void Reset_ISR(void) {
-    // Copy .data section to SRAM
-    uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
-    uint8_t *pDestination = (uint8_t*)&_sdata;  // SRAM
-    uint8_t *pSource = (uint8_t*)&_etext;   // FLASH
+  // Copy .data section to SRAM
+  uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
+  uint8_t *pDestination = (uint8_t*)&_sdata;  // SRAM
+  uint8_t *pSource = (uint8_t*)&_etext;   // FLASH
 
-    asm volatile(
-        "mov r0, %[pDestination]\n"     // Load the start address of the destination into r0
-        "mov r1, %[size]\n"             // Load the size of the copy into r1
-        "mov r2, #0\n"                  // Initialize r2 with zero
+  asm volatile(
+    "mov r0, %[pDestination]\n"     // Load the start address of the destination into r0
+    "mov r1, %[size]\n"             // Load the size of the copy into r1
+    "mov r2, #0\n"                  // Initialize r2 with zero
 
-        "copy_loop:\n"
-            "cmp r1, #0\n"              // Compare the size with zero
-            "beq copy_end\n"            // Branch to copy_end if size is zero
-            "ldrb r3, [%[pSource]]\n"   // Load a byte from the source address
-            "strb r3, [r0], #1\n"       // Store the byte to the destination address and increment the destination pointer
-            "subs r1, r1, #1\n"         // Subtract 1 from size
-            "b copy_loop\n"             // Branch back to copy_loop
+    "copy_loop:\n"
+      "cmp r1, #0\n"              // Compare the size with zero
+      "beq copy_end\n"            // Branch to copy_end if size is zero
+      "ldrb r3, [%[pSource]]\n"   // Load a byte from the source address
+      "strb r3, [r0], #1\n"       // Store the byte to the destination address and increment the destination pointer
+      "subs r1, r1, #1\n"         // Subtract 1 from size
+      "b copy_loop\n"             // Branch back to copy_loop
 
-        "copy_end:\n"
-        :
-        : [pDestination] "r" (pDestination), [size] "r" (size), [pSource] "r" (pSource)
-        : "r0", "r1", "r2", "r3"
-    );
+    "copy_end:\n"
+    :
+    : [pDestination] "r" (pDestination), [size] "r" (size), [pSource] "r" (pSource)
+    : "r0", "r1", "r2", "r3"
+  );
 
-    // Initialize .bss section to zero
-    size = (uint32_t)&_ebss - (uint32_t)&_sbss;
-    pDestination = (uint8_t*)&_sbss;
-        
-    asm volatile(
-        "mov r0, %[dest]\n"             // Load the start address of .bss section into r0
-        "mov r1, %[size]\n"             // Load the size of .bss section into r1
-        "mov r2, #0\n"                  // Initialize r2 with zero
+  // Initialize .bss section to zero
+  size = (uint32_t)&_ebss - (uint32_t)&_sbss;
+  pDestination = (uint8_t*)&_sbss;
+      
+  asm volatile(
+    "mov r0, %[dest]\n"             // Load the start address of .bss section into r0
+    "mov r1, %[size]\n"             // Load the size of .bss section into r1
+    "mov r2, #0\n"                  // Initialize r2 with zero
 
-        "loop:\n"
-            "cmp r1, #0\n"              // Compare the size with zero
-            "beq end\n"                 // Branch to end if size is zero
-            "strb r2, [r0], #1\n"       // Store byte zero at the current address and increment the address
-            "subs r1, r1, #1\n"         // Subtract 1 from size
-            "b loop\n"                  // Branch back to the loop
+    "loop:\n"
+      "cmp r1, #0\n"              // Compare the size with zero
+      "beq end\n"                 // Branch to end if size is zero
+      "strb r2, [r0], #1\n"       // Store byte zero at the current address and increment the address
+      "subs r1, r1, #1\n"         // Subtract 1 from size
+      "b loop\n"                  // Branch back to the loop
 
-        "end:\n"
-        :
-        : [dest] "r" (pDestination), [size] "r" (size)
-        : "r0", "r1", "r2"
-    );
+    "end:\n"
+    :
+    : [dest] "r" (pDestination), [size] "r" (size)
+    : "r0", "r1", "r2"
+  );
 
-    // Call program entry point
-    main();
+  // Call program entry point
+  main();
+}
+
+void NMI_ISR(void) {
+  while(1);
+}
+
+void HardFault_ISR(void) {
+  while(1);
+}
+
+void MemManage_ISR(void) {
+  while(1);
+}
+
+void BusFault_ISR(void) {
+  while(1);
+}
+
+void UsageFault_ISR(void) {
+  while(1);
 }
 
 /**
@@ -324,5 +344,5 @@ void Reset_ISR(void) {
  * It enters an infinite loop, effectively halting the system when an unexpected interrupt occurs.
  */
 void Default_ISR(void) {
-    while(1);
+  while(1);
 }
