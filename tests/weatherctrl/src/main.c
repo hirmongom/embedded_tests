258b078059f702495e122b774ff611cd474bf3eb
Hiram Montejano Gomez <hiram.montejano@gmail.com>
2023-07-26 09:28:20 +0200

    template for next project

---
+++ tests/weatherctrl/src/main.c
@@ -0,0 +1,13 @@
+/***************************************************************************************************
+ * @file        main.c
+ * 
+ * @author      Hiram Montejano Gómez (hiram.montejano@gmail.com)
+ * 
+ * @brief       
+ * 
+ * @details     
+ * 
+ * @date        26/07/2023
+ * 
+ * 
+ */
